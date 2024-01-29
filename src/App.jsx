3b9d36505ac2c5dd775f1fe254f9a857f63893e2
shm rsuf <shmusuf@gmail.com>
2024-01-29 19:53:54 +0600

    initial commit of m-4

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Form from "./components/Form";
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center mt-20 gap-5'>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
