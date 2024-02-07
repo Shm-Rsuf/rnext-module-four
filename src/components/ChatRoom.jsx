6db33d66de0959346e23d011b0b2ada1a0135c1c
shm rsuf <shmusuf@gmail.com>
2024-02-07 23:26:39 +0600

    react to reactive value added

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,11 +1,9 @@
-
 /* eslint-disable react/prop-types */
 import { useEffect } from "react";
 import { createConnection, visitLog } from "../utils/chat";
 
 // const serverUrl = "https://localhost:1234";
 export default function ChatRoom({ roomId, serverUrl }) {
-  console.log(location.pathname);
   useEffect(() => {
     const connection = createConnection(serverUrl, roomId);
     connection.connect();
