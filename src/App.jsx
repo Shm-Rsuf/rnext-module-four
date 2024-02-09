414752a46d661e73501e0197c63e0f28a6d95427
shm rsuf <shmusuf@gmail.com>
2024-02-09 11:19:33 +0600

    seperating event 4.20 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import { useState } from "react";
-import Page from "./components/Page";
-import { ShoppingCartContext } from "./contexts/shoppingCartContext";
+import Pointer from "./components/Pointer";
+import Pointer2 from "./components/Pointer2";
 
-const items = [
-  {
-    id: 1,
-    title: "Product 1",
-  },
-];
-
-export default function App() {
-  const [page, setPage] = useState("/home");
-  const [cartItems, setCartItems] = useState(items);
-
-  const handlePageChange = () => {
-    setPage("/cart");
-  };
-
-  const addItem = () => {
-    setCartItems([
-      ...cartItems,
-      {
-        id: 2,
-        title: "Product 2",
-      },
-    ]);
-  };
-
+const App = () => {
   return (
     <div>
-      <ShoppingCartContext.Provider value={cartItems}>
-        <Page url={page} onPageChange={handlePageChange} onAddItems={addItem} />
-      </ShoppingCartContext.Provider>
+      <Pointer2 />
+      <Pointer />
     </div>
   );
-}
+};
+
+export default App;
